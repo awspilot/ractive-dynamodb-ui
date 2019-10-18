57223531873b1cf757f6c6df760fa597d2630f2b
Awspilot <awspilot@192-168-0-149.rdsnet.ro>
2019-10-18 17:19:55 +0300

    +

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	node: false,
+	mode: 'production',
+    target: 'web',
+    context: path.resolve(__dirname, 'src'),
+    optimization: {
+        minimize: true,
+        minimizer: [new UglifyJsPlugin({
+            include: /\.min\.js$/
+        })]
+    },
+    plugins: [
+    ],
+    entry: {
+        'ractive-dynamodb-ui': path.resolve(__dirname, './src/index.ractive.html'),
+        'ractive-dynamodb-ui.min': path.resolve(__dirname, './src/index.ractive.html')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        library: 'dynamodb-ui',
+
+        // var, this, window, umd
+        libraryTarget: 'umd',
+        libraryExport: 'default',
+        umdNamedDefine: true   // Important
+    },
+    externals: {
+    	ractive: {
+    		commonjs: 'ractive',
+    		commonjs2: 'ractive',
+    		amd: 'ractive',
+    		root: 'Ractive'
+    	}
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ractive\.html$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: 'ractive-bin-loader'
+                    }
+                ]
+            }
+        ]
+    }
+}
