88976b007dd01163f71de54f7e6513d208ca32bf
adrianpraja <adrian@databank.ro>
2019-10-22 13:40:55 +0300

    update

--- src/itemview.js
+++ src/itemview.js
@@ -2,6 +2,8 @@
 
 //var jsoneditor = require('@awspilot/ractive-dynamodb-json-editor');
 import jsoneditor from '@awspilot/ractive-dynamodb-json-editor';
+import cloneDeep from 'lodash/cloneDeep';
+
 
 export default Ractive.extend({
 	isolated: true,
@@ -10,41 +12,89 @@ export default Ractive.extend({
 	},
 	template:
 		'\
-		<jsoneditor item={{rawitem}} navigationBar="{{false}}" style="position: absolute;top: 10px;left: 10px;right: 10px;bottom: 50px;width: auto;height: auto;border: 1px solid #97b0f8" menu-style="background-color: #d5ddf6;border-bottom: 1px solid #97b0f8;color: #444;" />\
+		<jsoneditor item={{itemtoedit}} navigationBar="{{false}}" convert_uint8_to_base64="{{true}}" style="position: absolute;top: 10px;left: 10px;right: 10px;bottom: 50px;width: auto;height: auto;border: 1px solid #97b0f8" menu-style="background-color: #d5ddf6;border-bottom: 1px solid #97b0f8;color: #444;" />\
 		<div style="position: absolute;left: 0px;right:10px;bottom:10px;height: 30px;box-sizing: border-box;">\
-			<a class="btn btn-sm btn-primary pull-right" on-click="create-item">Save</a>\
+			<a class="btn btn-sm btn-primary pull-right" on-click="update-item">Save</a>\
 		</div>\
 		',
 	data: function() {
 		return {
-
+			itemtoedit: {}
 		}
 	},
-
+	_hash_key_name: function() {
+		return (this.get('describeTable').KeySchema.filter(function(k) { return k.KeyType === 'HASH'})[0] || {}).AttributeName
+	},
+	_range_key_name: function() {
+		return (this.get('describeTable').KeySchema.filter(function(k) { return k.KeyType === 'RANGE'})[0] || {}).AttributeName;
+	},
 	oninit: function() {
 		var ractive = this
-		//console.log("createItem",  )
+
+		//this.set({itemtoedit: Object.assign({}, this.get('rawitem'))  })
+
+		this.set({itemtoedit: cloneDeep(this.get('rawitem'))  })
+
+
+
+		//var rawitem = this.get('rawitem')
+		//this.set({itemtoedit: { ...rawitem }  })
+
+
+
+		var debug = this.get('itemtoedit');
+		console.log("debug", debug )
+		//console.log("debug converted", btoa(String.fromCharCode.apply(null, debug.binary.B )) )
+
+
+		this.on('update-item', function() {
+			//console.log("table=","HASH=", this._hash_key_name(), " RANGE=", this._range_key_name() )
+
+			var originalitem = this.get('rawitem')
+			var updateditem = JSON.parse(JSON.stringify(this.get('itemtoedit')))
+
+			console.log("originalitem", originalitem.binary, typeof originalitem.binary )
+
+			var updateItemCall = {
+				TableName: this.get('describeTable.TableName'),
+				Key: {},
+				AttributeUpdates: {}
+			}
+
+			if ( !updateditem.hasOwnProperty( this._hash_key_name() ) )
+				return alert('Missing PARTITION_KEY ' + this._hash_key_name() )
+
+			if ( this._range_key_name() && (!updateditem.hasOwnProperty( this._range_key_name() )) )
+				return alert('Missing SORT_KEY ' + this._range_key_name() )
+
+			if ( JSON.stringify(updateditem[this._hash_key_name()]) !== JSON.stringify(originalitem[ this._hash_key_name()]) )
+				return alert('PARTITION_KEY(' + this._hash_key_name() + ') changed value. Operation not permitted')
+
+			if ( this._range_key_name() && ( JSON.stringify(updateditem[this._range_key_name()]) !== JSON.stringify(originalitem[ this._range_key_name()]) ) )
+				return alert('SORT_KEY(' + this._range_key_name() + ') changed value. Operation not permitted')
+
+			updateItemCall.Key[this._hash_key_name()] = updateditem[this._hash_key_name()]
+			delete updateditem[this._hash_key_name()]
+
+			if ( this._range_key_name() ) {
+				updateItemCall.Key[this._range_key_name()] = updateditem[this._range_key_name()]
+				delete updateditem[this._range_key_name()]
+			}
+
+			Object.keys(updateditem).map(function( k ) {
+				updateItemCall.AttributeUpdates[k] = {
+					Action: 'PUT', //
+					Value: updateditem[k],
+				}
+			})
+
+			//console.log("should update item", this.get('item') )
+			console.log("updateItem", updateItemCall  )
+			DynamoDB.client.updateItem(updateItemCall, function(err,data) {
+				console.log(err,data)
+			})
+		})
+
 	},
-	oncomplete: function() {
-		//console.log("will show rawitem", this.get('rawitem') )
-		// var ractive = this;
-		// var container = document.getElementById('jsoneditor');
-		// var options = {
-		// 	//statusBar
-		// 	//mainMenuBar
-		// 	history: false,
-		// 	colorPicker: false,
-		// 	//timestampTag
-		// 	autocomplete: false,
-		// 	navigationBar: false,
-		// 	search: false,
-		// 	enableSort: false,
-		// 	sortObjectKeys: false,
-		// 	enableTransform: false,
-		//
-		// 	mode: 'view',
-		// };
-		//ractive.editor = new JSONEditor(container, options);
-		//ractive.editor.set(ractive.get('item'))
-	}
+
 })
