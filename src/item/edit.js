50b6881aabe9e91989d9c43d4186b4289f8a1964
adrianpraja <adrian@databank.ro>
2019-10-23 18:20:31 +0300

    removed bootstrap

--- src/item/edit.js
+++ src/item/edit.js
@@ -15,7 +15,7 @@ export default Ractive.extend({
 		<jsoneditor item={{itemtoedit}} navigationBar="{{false}}"  style="position: absolute;top: -1px;left: -1px;right: -1px;bottom: 50px;width: auto;height: auto;border: 1px solid #dadada" menu-style="background-color: #d5ddf6;border-bottom: 1px solid #97b0f8;color: #444;" />\
 		<div style="position: absolute;left: 10px;right:10px;bottom:10px;height: 30px;box-sizing: border-box;">\
 			<span style="color: red;line-height: 30px;">{{errorMessage}}</span>\
-			<a class="btn btn-sm btn-primary pull-right" on-click="update-item">Save</a>\
+			<a class="btn btn-sm btn-primary pull-right" style="color: #fff;background-color: #337ab7;border-color: #2e6da4;float: right;padding: 5px 10px; font-size: 12px; line-height: 1.5;border-radius: 3px;font-family: sans-serif;" on-click="update-item">Save</a>\
 		</div>\
 		',
 	data: function() {
