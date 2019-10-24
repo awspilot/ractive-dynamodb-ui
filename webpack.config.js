b6816fe5af73b2120b7e212af98fb1d94cd2b60e
adrianpraja <adrian@databank.ro>
2019-10-24 11:06:58 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -5,51 +5,51 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
 	node: false,
 	mode: 'production',
-    target: 'web',
-    context: path.resolve(__dirname, 'src'),
-    optimization: {
+	target: 'web',
+	context: path.resolve(__dirname, 'src'),
+	optimization: {
 		minimize: true,
 		minimizer: [new UglifyJsPlugin({
 			uglifyOptions: { ecma: 7 },
 			sourceMap: true,
 			include: /\.min\.js$/
 		})]
-    },
-    plugins: [
-    ],
-    entry: {
-        'ractive-dynamodb-ui': path.resolve(__dirname, './src/index.ractive.html'),
-        'ractive-dynamodb-ui.min': path.resolve(__dirname, './src/index.ractive.html')
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        library: 'dynamodb-ui',
+	},
+	plugins: [
+	],
+	entry: {
+		'ractive-dynamodb-ui': path.resolve(__dirname, './src/index.ractive.html'),
+		'ractive-dynamodb-ui.min': path.resolve(__dirname, './src/index.ractive.html')
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js',
+		library: 'dynamodb-ui',
 
-        // var, this, window, umd
-        libraryTarget: 'umd',
-        libraryExport: 'default',
-        umdNamedDefine: true   // Important
-    },
-    externals: {
-    	ractive: {
-    		commonjs: 'ractive',
-    		commonjs2: 'ractive',
-    		amd: 'ractive',
-    		root: 'Ractive'
-    	}
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ractive\.html$/,
-                exclude: /(node_modules|bower_components)/,
-                use: [
-                    {
-                        loader: 'ractive-bin-loader'
-                    }
-                ]
-            }
-        ]
-    }
+		// var, this, window, umd
+		libraryTarget: 'umd',
+		libraryExport: 'default',
+		umdNamedDefine: true   // Important
+	},
+	externals: {
+		ractive: {
+			commonjs: 'ractive',
+			commonjs2: 'ractive',
+			amd: 'ractive',
+			root: 'Ractive'
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ractive\.html$/,
+				exclude: /(node_modules|bower_components)/,
+				use: [
+					{
+						loader: 'ractive-bin-loader'
+					}
+				]
+			}
+		]
+	}
 }
