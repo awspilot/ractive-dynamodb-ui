b5b7b9e8c04cf375fec0cbcaec688d25815d4aa9
adrianpraja <adrian@databank.ro>
2019-10-30 09:37:27 +0200

    update

--- src/table/capacity.js
+++ src/table/capacity.js
@@ -5,7 +5,7 @@ export default Ractive.extend({
 			<div style='padding: 30px'>
 				<h3>
 					Provisioned capacity
-					<a class='btn btn-xs btn-default pull-right' on-click='refresh-table'><i class='icon zmdi zmdi-refresh'></i></a>
+					<a class='btn btn-sm btn-default pull-right' on-click='refresh-table'><i class='icon zmdi zmdi-refresh'></i></a>
 				</h3>
 				<hr>
 
