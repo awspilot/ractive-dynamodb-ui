ee21cd781d8731c6a0222ffc6b571d6850e225bf
adrianpraja <adrian@databank.ro>
2019-10-30 09:19:44 +0200

    show loading in table capacity

--- src/table/capacity.js
+++ src/table/capacity.js
@@ -8,32 +8,34 @@ export default Ractive.extend({
 					<a class='btn btn-xs btn-default pull-right' on-click='refresh-table'><i class='icon zmdi zmdi-refresh'></i></a>
 				</h3>
 				<hr>
-					{{#if describeTable.BillingModeSummary.BillingMode === 'PAY_PER_REQUEST'}}
-						Not applicable because read/write capacity mode is on-demand.<br>
+
+					{{#if localDescribeTable === false}}
+						Loading...
 					{{else}}
-						<table>
-							<tr>
-								<td width='160' align='right'></td>
-								<td width='160'>Read capacity units</td>
-								<td width='160'>Write capacity units</td>
-							</tr>
-							<tr>
-								<td>Table</td>
-								<td><input type='text' size='4' value='{{describeTable.ProvisionedThroughput.ReadCapacityUnits}}' on-focus='focus' /></td>
-								<td><input type='text' size='4' value='{{describeTable.ProvisionedThroughput.WriteCapacityUnits}}' on-focus='focus' /></td>
-							</tr>
-							{{#describeTable.GlobalSecondaryIndexes}}
-							<tr>
-								<td>{{ .IndexName }}</td>
-								<td><input type='text' size='4' value='{{.ProvisionedThroughput.ReadCapacityUnits}}' on-focus='focus' /></td>
-								<td><input type='text' size='4' value='{{.ProvisionedThroughput.WriteCapacityUnits}}' on-focus='focus' /></td>
-							</tr>
-							{{/describeTable.GlobalSecondaryIndexes}}
-						</table>
+						{{#if localDescribeTable.BillingModeSummary.BillingMode === 'PAY_PER_REQUEST'}}
+							Not applicable because read/write capacity mode is on-demand.<br>
+						{{else}}
+							<table>
+								<tr>
+									<td width='160' align='right'></td>
+									<td width='160'>Read capacity units</td>
+									<td width='160'>Write capacity units</td>
+								</tr>
+								<tr>
+									<td>Table</td>
+									<td><input type='text' size='4' value='{{localDescribeTable.ProvisionedThroughput.ReadCapacityUnits}}' on-focus='focus' /></td>
+									<td><input type='text' size='4' value='{{localDescribeTable.ProvisionedThroughput.WriteCapacityUnits}}' on-focus='focus' /></td>
+								</tr>
+								{{#localDescribeTable.GlobalSecondaryIndexes}}
+								<tr>
+									<td>{{ .IndexName }}</td>
+									<td><input type='text' size='4' value='{{.ProvisionedThroughput.ReadCapacityUnits}}' on-focus='focus' /></td>
+									<td><input type='text' size='4' value='{{.ProvisionedThroughput.WriteCapacityUnits}}' on-focus='focus' /></td>
+								</tr>
+								{{/localDescribeTable.GlobalSecondaryIndexes}}
+							</table>
+						{{/if}}
 					{{/if}}
-
-
-
 				<h3>Auto Scaling</h3>
 				<hr/>
 					<small>Auto Scaling not supported by this UI</small>
@@ -54,14 +56,14 @@ export default Ractive.extend({
 	oninit: function() {
 		var ractive = this;
 		var refresh_table = function() {
-			ractive.set('describeTable', {})
+			ractive.set('localDescribeTable', false )
 			DynamoDB.client.describeTable( { TableName: ractive.get('table.name') }, function(err, data) {
 				if (err)
 					return ractive.set('err', err.message );
 
-				console.log(data)
-				ractive.set('describeTable', data.Table)
-				ractive.set('originalDescribeTable', JSON.parse(JSON.stringify( data.Table ))) // this wont change
+				//console.log(data)
+				ractive.set('localDescribeTable', data.Table)
+				//ractive.set('originalDescribeTable', JSON.parse(JSON.stringify( data.Table ))) // this wont change
 			})
 		}
 		ractive.on('cancel', function() {
@@ -80,12 +82,12 @@ export default Ractive.extend({
 			};
 
 			if (
-				(ractive.get('describeTable.ProvisionedThroughput.ReadCapacityUnits')  !== ractive.get('originalDescribeTable.ProvisionedThroughput.ReadCapacityUnits') ) ||
-				(ractive.get('describeTable.ProvisionedThroughput.WriteCapacityUnits') !== ractive.get('originalDescribeTable.ProvisionedThroughput.WriteCapacityUnits') )
+				(ractive.get('localDescribeTable.ProvisionedThroughput.ReadCapacityUnits')  !== ractive.get('originalDescribeTable.ProvisionedThroughput.ReadCapacityUnits') ) ||
+				(ractive.get('localDescribeTable.ProvisionedThroughput.WriteCapacityUnits') !== ractive.get('originalDescribeTable.ProvisionedThroughput.WriteCapacityUnits') )
 			) {
 				payload.ProvisionedThroughput = {
-					ReadCapacityUnits: ractive.get('describeTable.ProvisionedThroughput.ReadCapacityUnits'),
-					WriteCapacityUnits: ractive.get('describeTable.ProvisionedThroughput.WriteCapacityUnits'),
+					ReadCapacityUnits: ractive.get('localDescribeTable.ProvisionedThroughput.ReadCapacityUnits'),
+					WriteCapacityUnits: ractive.get('localDescribeTable.ProvisionedThroughput.WriteCapacityUnits'),
 				}
 			} else {
 				// if no changes, do not add obj at all
@@ -94,9 +96,9 @@ export default Ractive.extend({
 
 
 			// each index
-			if ((ractive.get('describeTable.GlobalSecondaryIndexes') || []).length) {
+			if ((ractive.get('localDescribeTable.GlobalSecondaryIndexes') || []).length) {
 				payload.GlobalSecondaryIndexUpdates = []
-				ractive.get('describeTable.GlobalSecondaryIndexes').map(function(gsi, i ) {
+				ractive.get('localDescribeTable.GlobalSecondaryIndexes').map(function(gsi, i ) {
 					var original_gsi = ractive.get('originalDescribeTable.GlobalSecondaryIndexes.'+i )
 
 					console.log("gsi",gsi)
