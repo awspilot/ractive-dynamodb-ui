9a41a34210e8946cc529cf05289ab16772cc68d5
adrianpraja <adrian@databank.ro>
2019-12-30 12:29:13 +0200

    +

---
+++ webpack.dev.js
@@ -0,0 +1,71 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	node: false,
+	mode: 'development',
+	devServer: {
+		//contentBase: path.join(__dirname, 'demo'),
+		//compress: true,
+		port: 8888,
+		writeToDisk: true,
+	},
+
+	target: 'web',
+	context: path.resolve(__dirname, 'src'),
+	optimization: {
+		minimize: true,
+		minimizer: [new UglifyJsPlugin({
+			//uglifyOptions: { ecma: 7 },
+			//sourceMap: true,
+			include: /\.min\.js$/
+		})]
+	},
+	plugins: [
+	],
+	entry: {
+		'ractive-dynamodb-ui': path.resolve(__dirname, './src/index.ractive.html'),
+		'ractive-dynamodb-ui.min': path.resolve(__dirname, './src/index.ractive.html')
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js',
+		library: 'ractive-dynamodb-ui',
+
+		// var, this, window, umd , amd, commonjs, global
+		libraryTarget: 'umd',
+		libraryExport: 'default',
+		umdNamedDefine: true   // Important
+	},
+	externals: {
+		ractive: {
+			commonjs: 'ractive',  // require
+			commonjs2: 'ractive', // require + module.exports - used by nodejs
+			amd: 'ractive',
+			root: 'Ractive'
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: 'babel-loader'
+			},
+			{
+					test: /\.ractive\.html$/,
+					use: 'babel-loader'
+			},
+			{
+				test: /\.ractive\.html$/,
+				exclude: /(node_modules|bower_components)/,
+				use: [
+					{
+						loader: 'ractive-bin-loader'
+					}
+				]
+			}
+		]
+	}
+}
