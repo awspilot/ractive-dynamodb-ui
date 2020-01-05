c45ef0e40eab712b63fa09c4b2e6f8bb4c4d15c5
adrianpraja <adrian@databank.ro>
2020-01-05 11:22:16 +0200

    update

--- webpack.dev.js
+++ webpack.dev.js
@@ -40,7 +40,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: '[name].js',
-		library: 'ractive-dynamodb-ui',
+		library: '@awspilot/ractive-dynamodb-ui',
 
 		// var, this, window, umd , amd, commonjs, global
 		libraryTarget: 'umd',
