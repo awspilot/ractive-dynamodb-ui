fc64fc303fce7e915c1eba7b3b21fba4f5117e2d
Awspilot <awspilot@192-168-0-136.rdsnet.ro>
2020-01-07 10:46:54 +0200

    +

--- src/table/_utils.js
+++ src/table/_utils.js
@@ -61,6 +61,31 @@ var _gsi_hash_key_type_name = function( indexname ) {
 	return ({S: 'String', N: 'Number', 'B': 'Binary'})[ this._gsi_hash_key_type( indexname ) ]
 }
 
+var _gsi_range_key_name = function( indexname ) {
+
+	var index = (this.get('describeTable.GlobalSecondaryIndexes') || []).filter(function(i) {return i.IndexName === indexname})[0];
+	if (! index )
+		return;
+
+	return (index.KeySchema.filter(function(k) { return k.KeyType === 'RANGE'})[0] || {}).AttributeName
+
+}
+
+var _gsi_range_key_type = function( indexname ) {
+	var ractive = this;
+
+	var ret;
+	this.get('describeTable.AttributeDefinitions').map(function( at ) {
+		if ( at.AttributeName === ractive._gsi_range_key_name( indexname ) )
+			ret = at.AttributeType
+	})
+	return ret;
+}
+
+var _gsi_range_key_type_name = function( indexname ) {
+	return ({S: 'String', N: 'Number', 'B': 'Binary'})[ this._gsi_range_key_type( indexname ) ]
+}
+
 export default {
 	_hash_key_name: _hash_key_name,
 	_hash_key_type: _hash_key_type,
@@ -73,4 +98,9 @@ export default {
 	_gsi_hash_key_name: _gsi_hash_key_name,
 	_gsi_hash_key_type: _gsi_hash_key_type,
 	_gsi_hash_key_type_name: _gsi_hash_key_type_name,
+
+	_gsi_range_key_name: _gsi_range_key_name,
+	_gsi_range_key_type: _gsi_range_key_type,
+	_gsi_range_key_type_name: _gsi_range_key_type_name,
+
 }
