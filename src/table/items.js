2ed0eb285b4413beb7b0284147add02e14563db7
Awspilot <awspilot@192-168-0-136.rdsnet.ro>
2020-01-07 10:52:23 +0200

    split code

--- src/table/items.js
+++ src/table/items.js
@@ -1,4 +1,6 @@
 
+import _table_utils from './_utils';
+
 import tabledata from '../tabledata';
 import itemedit from '../item/edit';
 import itemadd from '../item/add';
@@ -227,88 +229,25 @@ export default Ractive.extend({
 		{{/if}}
 	</div>
 		`,
-		_hash_key_name: function() {
-			return (this.get('describeTable').KeySchema.filter(function(k) { return k.KeyType === 'HASH'})[0] || {}).AttributeName
-		},
-		_hash_key_type: function() {
-			var ractive = this;
 
-			var ret;
-			this.get('describeTable.AttributeDefinitions').map(function( at ) {
-				if ( at.AttributeName === ractive._hash_key_name() )
-					ret = at.AttributeType
-			})
-			return ret;
-		},
-		_hash_key_type_name: function() {
-			return ({S: 'String', N: 'Number', 'B': 'Binary'})[ this._hash_key_type() ]
-		},
+		_hash_key_name: _table_utils._hash_key_name,
+		_hash_key_type: _table_utils._hash_key_type,
+		_hash_key_type_name: _table_utils._hash_key_type_name,
 
-		_range_key_name: function() {
-			return (this.get('describeTable').KeySchema.filter(function(k) { return k.KeyType === 'RANGE'})[0] || {}).AttributeName;
-		},
-		_range_key_type: function() {
-			var ractive = this;
-
-			var ret;
-			this.get('describeTable.AttributeDefinitions').map(function( at ) {
-				if ( at.AttributeName === ractive._range_key_name() )
-					ret = at.AttributeType
-			})
-			return ret;
-		},
-		_range_key_type_name: function() {
-			return ({S: 'String', N: 'Number', 'B': 'Binary'})[ this._range_key_type() ]
-		},
+		_range_key_name: _table_utils._range_key_name,
+		_range_key_type: _table_utils._range_key_type,
+		_range_key_type_name: _table_utils._range_key_type_name,
 
 
-		_gsi_hash_key_name: function( indexname ) {
-
-			var index = (this.get('describeTable.GlobalSecondaryIndexes') || []).filter(function(i) {return i.IndexName === indexname})[0];
-			if (! index )
-				return;
-
-			return (index.KeySchema.filter(function(k) { return k.KeyType === 'HASH'})[0] || {}).AttributeName
-
-		},
-		_gsi_hash_key_type: function( indexname ) {
-			var ractive = this;
-
-			var ret;
-			this.get('describeTable.AttributeDefinitions').map(function( at ) {
-				if ( at.AttributeName === ractive._gsi_hash_key_name( indexname ) )
-					ret = at.AttributeType
-			})
-			return ret;
-		},
-		_gsi_hash_key_type_name: function( indexname ) {
-			return ({S: 'String', N: 'Number', 'B': 'Binary'})[ this._gsi_hash_key_type( indexname ) ]
-		},
+		_gsi_hash_key_name: _table_utils._gsi_hash_key_name,
+		_gsi_hash_key_type: _table_utils._gsi_hash_key_type,
+		_gsi_hash_key_type_name: _table_utils._gsi_hash_key_type_name,
 
 
 
-		_gsi_range_key_name: function( indexname ) {
-
-			var index = (this.get('describeTable.GlobalSecondaryIndexes') || []).filter(function(i) {return i.IndexName === indexname})[0];
-			if (! index )
-				return;
-
-			return (index.KeySchema.filter(function(k) { return k.KeyType === 'RANGE'})[0] || {}).AttributeName
-
-		},
-		_gsi_range_key_type: function( indexname ) {
-			var ractive = this;
-
-			var ret;
-			this.get('describeTable.AttributeDefinitions').map(function( at ) {
-				if ( at.AttributeName === ractive._gsi_range_key_name( indexname ) )
-					ret = at.AttributeType
-			})
-			return ret;
-		},
-		_gsi_range_key_type_name: function( indexname ) {
-			return ({S: 'String', N: 'Number', 'B': 'Binary'})[ this._gsi_range_key_type( indexname ) ]
-		},
+		_gsi_range_key_name: _table_utils._gsi_range_key_name,
+		_gsi_range_key_type: _table_utils._gsi_range_key_type,
+		_gsi_range_key_type_name: _table_utils._gsi_range_key_type_name,
 
 
 
@@ -320,53 +259,14 @@ export default Ractive.extend({
 
 
 
-		_lsi_hash_key_name: function( indexname ) {
-
-			var index = (this.get('describeTable.LocalSecondaryIndexes') || []).filter(function(i) {return i.IndexName === indexname})[0];
-			if (! index )
-				return;
-
-			return (index.KeySchema.filter(function(k) { return k.KeyType === 'HASH'})[0] || {}).AttributeName
-
-		},
-		_lsi_hash_key_type: function( indexname ) {
-			var ractive = this;
-
-			var ret;
-			this.get('describeTable.AttributeDefinitions').map(function( at ) {
-				if ( at.AttributeName === ractive._lsi_hash_key_name( indexname ) )
-					ret = at.AttributeType
-			})
-			return ret;
-		},
-		_lsi_hash_key_type_name: function( indexname ) {
-			return ({S: 'String', N: 'Number', 'B': 'Binary'})[ this._lsi_hash_key_type( indexname ) ]
-		},
+		_lsi_hash_key_name: _table_utils._lsi_hash_key_name,
+		_lsi_hash_key_type: _table_utils._lsi_hash_key_type,
+		_lsi_hash_key_type_name: _table_utils._lsi_hash_key_type_name,
 
 
-
-		_lsi_range_key_name: function( indexname ) {
-
-			var index = (this.get('describeTable.LocalSecondaryIndexes') || []).filter(function(i) {return i.IndexName === indexname})[0];
-			if (! index )
-				return;
-
-			return (index.KeySchema.filter(function(k) { return k.KeyType === 'RANGE'})[0] || {}).AttributeName
-
-		},
-		_lsi_range_key_type: function( indexname ) {
-			var ractive = this;
-
-			var ret;
-			this.get('describeTable.AttributeDefinitions').map(function( at ) {
-				if ( at.AttributeName === ractive._lsi_range_key_name( indexname ) )
-					ret = at.AttributeType
-			})
-			return ret;
-		},
-		_lsi_range_key_type_name: function( indexname ) {
-			return ({S: 'String', N: 'Number', 'B': 'Binary'})[ this._lsi_range_key_type( indexname ) ]
-		},
+		_lsi_range_key_name: _table_utils._lsi_range_key_name,
+		_lsi_range_key_type: _table_utils._lsi_range_key_type,
+		_lsi_range_key_type_name: _table_utils._lsi_range_key_type_name,
 
 
 
