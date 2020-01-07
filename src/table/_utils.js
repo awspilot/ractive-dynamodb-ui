8731a04c822f152d5d0bd368a566681029d5d516
Awspilot <awspilot@192-168-0-136.rdsnet.ro>
2020-01-07 10:39:41 +0200

    +

--- src/table/_utils.js
+++ src/table/_utils.js
@@ -17,8 +17,14 @@ var _hash_key_type_name = function() {
 	return ({S: 'String', N: 'Number', 'B': 'Binary'})[ this._hash_key_type() ]
 }
 
+var _range_key_name = function() {
+	return (this.get('describeTable').KeySchema.filter(function(k) { return k.KeyType === 'RANGE'})[0] || {}).AttributeName;
+}
+
 export default {
 	_hash_key_name: _hash_key_name,
 	_hash_key_type: _hash_key_type,
 	_hash_key_type_name: _hash_key_type_name,
+
+	_range_key_name: _range_key_name,
 }
