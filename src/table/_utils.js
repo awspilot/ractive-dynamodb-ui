f46a2e4ffc636791d861f516eb85c17ba56274ca
Awspilot <awspilot@192-168-0-136.rdsnet.ro>
2020-01-07 10:40:53 +0200

    +

--- src/table/_utils.js
+++ src/table/_utils.js
@@ -21,10 +21,22 @@ var _range_key_name = function() {
 	return (this.get('describeTable').KeySchema.filter(function(k) { return k.KeyType === 'RANGE'})[0] || {}).AttributeName;
 }
 
+var _range_key_type = function() {
+	var ractive = this;
+
+	var ret;
+	this.get('describeTable.AttributeDefinitions').map(function( at ) {
+		if ( at.AttributeName === ractive._range_key_name() )
+			ret = at.AttributeType
+	})
+	return ret;
+}
+
 export default {
 	_hash_key_name: _hash_key_name,
 	_hash_key_type: _hash_key_type,
 	_hash_key_type_name: _hash_key_type_name,
 
 	_range_key_name: _range_key_name,
+	_range_key_type: _range_key_type,
 }
