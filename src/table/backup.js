44f5c4bbcb6ee4c94ee9e1fdf5c96d89e90abdfb
adrianpraja <adrian@databank.ro>
2020-01-10 10:40:44 +0200

    implemented backup delete

--- src/table/backup.js
+++ src/table/backup.js
@@ -19,7 +19,7 @@ export default Ractive.extend({
 				<div>
 					<a class='btn btn-sm btn-primary disabled' on-click='create'>Create backup</a>
 					<a class='btn btn-sm btn-default disabled' on-click='restore'>Restore backup</a>
-					<a class='btn btn-sm btn-default disabled' on-click='delete'>Delete backup</a>
+					<a class='btn btn-sm btn-default {{#if selection_size !== 1}}disabled{{/if}}' on-click='delete'>Delete backup</a>
 
 					<a class='btn btn-sm btn-default pull-right' on-click='refresh'><icon-refresh /></a>
 				</div>
@@ -41,14 +41,14 @@ export default Ractive.extend({
 
 			ractive.set('rows', data.BackupSummaries.map(function(b) {
 				return [
-					{ KEY: true },
+					{ KEY: true, item: b },
 					{ S: b.BackupName },
 					{ S: b.BackupStatus },
 					{ S: b.BackupCreationDateTime.toISOString().split('T').join(' ') },
 					{ S: Math.ceil((b.BackupSizeBytes)/1024) + 'K' },
 					{ S: b.BackupType },
 					{ S: '' },
-					{ S: b.TableArn },
+					{ S: b.BackupArn },
 					// { N: index.IndexSizeBytes.toString() },
 					// { N: index.ItemCount.toString() },
 
@@ -59,6 +59,43 @@ export default Ractive.extend({
 	},
 	oninit: function() {
 		var ractive=this;
+
+		ractive.on('tabledata.selectrow', function(context) {
+			var keypath = context.resolve()
+			ractive.set(keypath + '.0.selected', !ractive.get(keypath + '.0.selected') )
+			ractive.set('selection_size', ractive.get('rows').filter(function(r) { return r[0].selected === true } ).length )
+		})
+
+		ractive.on('delete', function() {
+			var selected = ractive.get('rows').filter(function(r) { return r[0].selected === true } );
+
+			if ( selected.length === 0 )
+				return alert('Please select a backup to delete')
+
+			if ( selected.length > 1 )
+				return alert('Please select one backup at a time')
+
+			var backup = ractive.get('rows').filter(function(r) { return r[0].selected === true } )[0];
+			var tablename = ractive.get('describeTable.TableName')
+
+			var backupname = backup[0].item.BackupName;
+
+			if (confirm('Are you sure you want to delete backup ' + backupname + ' of table ' + tablename )) {
+
+				var params = {
+					BackupArn: backup[0].item.BackupArn
+				};
+
+				DynamoDB.client.deleteBackup(params, function(err, data) {
+					if (err)
+						return alert( err.message );
+
+					ractive.list_backups()
+				});
+
+			}
+
+		})
 
 		this.on('refresh', function() {
 			ractive.list_backups()
